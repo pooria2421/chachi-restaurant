b7faa66698c1b470731b4a2ef9387076ae61cd38
pooria2421 <pooria2421@gmail.com>
2024-04-01 13:56:25 -0700

    pu

--- components/foodCol.js
+++ components/foodCol.js
@@ -4,6 +4,14 @@ import { useState } from "react";
 
 export default function FoodCol({productName,productPrice,productDescription,productStar , gallery}){
     const [value, setValue] = useState(2);
+    function toRial(str) {
+      str = str.replace(/\,/g, "");
+      var objRegex = new RegExp("(-?[0-9]+)([0-9]{3})");
+      while (objRegex.test(str)) {
+        str = str.replace(objRegex, "$1,$2");
+      }
+      return str;
+    }
     return (
         <div className="col-xs-12 col-lg-12 food-col d-flex mt-2 mb-2 p-2" style={{paddingLeft:'14px' , paddingRight:'14px',justifyContent:'space-between'}}>
             <div className="col-xs-5 col-lg-5" style={{display:'flex' , flexDirection:'column' , justifyContent:'space-around'}}>
@@ -18,7 +26,7 @@ export default function FoodCol({productName,productPrice,productDescription,pro
             <div className="col-7 d-flex" style={{justifyContent:'space-between'}}>
                 
           <div className="col-5" style={{display:'flex' , flexDirection:'column' , justifyContent:'center'}}>
-            <div className="food-col-price d-flex justify-content-between"><span style={{fontWeight:'bolder',borderRadius:'10px' , fontSize:'11px'}}>{productPrice}</span> <span className="d-flex align-items-center" style={{fontSize:'11px'}}>تومان</span></div>
+            <div className="food-col-price d-flex justify-content-between"><span style={{fontWeight:'bolder',borderRadius:'10px' , fontSize:'11px'}}>{toRial(productPrice.toString())}</span> <span className="d-flex align-items-center" style={{fontSize:'11px'}}>تومان</span></div>
             <div className="d-flex justify-content-between align-items-center mt-2">
             <span style={{fontSize:'12px'}}>سفارش</span>
             <span><img src="/assets/images/phoneRed.png" width={'12'}/></span>
