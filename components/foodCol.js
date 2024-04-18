cc8265af8444b3f9308f3d6fc9b8bf1518572263
pooria2421 <pooria2421@gmail.com>
2024-04-18 03:10:38 -0700

    change sells

--- components/foodCol.js
+++ components/foodCol.js
@@ -13,27 +13,25 @@ export default function FoodCol({productName,productPrice,productDescription,pro
       return str;
     }
     return (
-        <div className="col-12 food-col d-flex mt-2 mb-2 p-2" style={{paddingLeft:'14px' , paddingRight:'14px',justifyContent:'space-between'}}>
-            <div className="col-xs-5 col-lg-5" style={{display:'flex' , flexDirection:'column' , justifyContent:'space-around'}}>
+        <div className="col-12 food-col d-flex mt-2 mb-2 p-2" style={{paddingLeft:'14px' , paddingRight:'14px',justifyContent:'space-between',borderBottom:'0.5px solid #eee'}}>
+            <div className="col-7 mb-2" style={{display:'flex' , flexDirection:'column' , justifyContent:'space-between'}}>
           <div className="">
             <div className="food-col-title">{productName}</div>
             <div className="food-col-description">{productDescription}</div>
             </div>
             <div className="">
-            <Rating name="disabled" value={productStar} disabled />
+            <div className="col-6" style={{display:'flex' , flexDirection:'column' , justifyContent:'center'}}>
+            <div className="food-col-price d-flex justify-content-between"><span style={{fontWeight:'bolder',borderRadius:'10px' , fontSize:'12px'}}>{toRial(productPrice.toString())}</span> <span className="d-flex align-items-center" style={{fontSize:'11px'}}>تومان</span></div>
+          
+            </div>
+            {/* <Rating name="disabled" value={productStar} disabled /> */}
              </div>
             </div>
-            <div className="col-7 d-flex" style={{justifyContent:'space-between'}}>
+            <div className="col-5 d-flex" style={{justifyContent:'end'}}>
                 
-          <div className="col-4" style={{display:'flex' , flexDirection:'column' , justifyContent:'center'}}>
-            <div className="food-col-price d-flex justify-content-between"><span style={{fontWeight:'bolder',borderRadius:'10px' , fontSize:'12px'}}>{toRial(productPrice.toString())}</span> <span className="d-flex align-items-center" style={{fontSize:'11px'}}>تومان</span></div>
-            <div className="d-flex justify-content-between align-items-center mt-2">
-            <span style={{fontSize:'12px'}}>سفارش</span>
-            <span><img src="/assets/images/phoneRed.png" width={'12'}/></span>
-            </div>
-            </div>
+     
 {
-            <div className="food-col-image col-6" style={{background:`url(${baseURLImage+gallery[0] == "" || gallery[0] == undefined ? '' : baseURLImage+gallery[0].url})`,backgroundSize:'contain',backgroundRepeat:'no-repeat'}} ></div>
+            <div className="food-col-image col-12" style={{background:`url(${baseURLImage+gallery[0] == "" || gallery[0] == undefined ? '' : baseURLImage+gallery[0].url})`,backgroundSize:'contain',backgroundRepeat:'no-repeat'}} ></div>
 
             // <div className="food-col-image col-6" style={{background:`url(${baseURLImage+gallery[0].url})`,backgroundSize:'contain',backgroundRepeat:'no-repeat'}} ></div>
 }
